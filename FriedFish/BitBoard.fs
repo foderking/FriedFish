c21413d31ebdc10a11944050780471cea86b73a3
foderking <foderking@gmail.com>
2025-05-29 22:35:39 +0100

    indents

--- FriedFish/BitBoard.fs
+++ FriedFish/BitBoard.fs
@@ -3,24 +3,24 @@
 type BitBoard = uint64
   
 type Ranks =
-| RANK_1 = 0
-| RANK_2 = 1
-| RANK_3 = 2
-| RANK_4 = 3
-| RANK_5 = 4
-| RANK_6 = 5
-| RANK_7 = 6
-| RANK_8 = 7
+    | RANK_1 = 0
+    | RANK_2 = 1
+    | RANK_3 = 2
+    | RANK_4 = 3
+    | RANK_5 = 4
+    | RANK_6 = 5
+    | RANK_7 = 6
+    | RANK_8 = 7
 
 type Files =
-| FILE_A = 0
-| FILE_B = 1
-| FILE_C = 2
-| FILE_D = 3
-| FILE_E = 4
-| FILE_F = 5
-| FILE_G = 6
-| FILE_H = 7
+    | FILE_A = 0
+    | FILE_B = 1
+    | FILE_C = 2
+    | FILE_D = 3
+    | FILE_E = 4
+    | FILE_F = 5
+    | FILE_G = 6
+    | FILE_H = 7
  
 let createFromRankFile(rank: Ranks, file: Files): BitBoard =
     (uint64 rank) * 8UL + (uint64 file)
